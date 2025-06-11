839c3c517385c37dc8ef45b1ec6a678700da5a64
Suparna Das <suparna.math@gmail.com>
2025-06-11 19:08:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ html, body, [class*="css"] {
 
 # ─────────────── 3. API Keys and LLM ───────────────
 os.environ["GROQ_API_KEY"] = "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"
-NASA_API_KEY = "rD8cgucyU9Rgcn1iTaOeh7mo1CPd6oN4CYThCdjg"
+#NASA_API_KEY = "rD8cgucyU9Rgcn1iTaOeh7mo1CPd6oN4CYThCdjg"
+NASA_API_KEY = "ANQ8BVHQKuI1iYWqjUJhgLNzooitLCiZ2nvVD2gB"
 
 embed_model = HuggingFaceEmbedding(model_name="all-MiniLM-L6-v2", device="cpu")
 Settings.embed_model = embed_model
