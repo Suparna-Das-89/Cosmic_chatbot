fcd0522df2d2aeb3170ee59b059f925d8b64d206
Suparna Das <suparna.math@gmail.com>
2025-06-11 17:35:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 import ephem
-from llama_index.core.schema import Document
 from llama_index.core import VectorStoreIndex, Settings
+from llama_index.core.schema import Document
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
 from sentence_transformers import SentenceTransformer, util
